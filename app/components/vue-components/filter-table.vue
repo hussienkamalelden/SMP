50f14e88dfa0a330f4928ce67eed21f2b9ee3baf
Hussien Kamal <Hussien.Kamal@linkdev.com>
2022-06-17 22:14:54 +0200

    Finish #Req1

--- app/components/vue-components/filter-table.vue
+++ app/components/vue-components/filter-table.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <form class="form-holder" id="search">
-            <input class="search-bar" name="query" v-on:keyup="Search(Message)" v-model="Message" placeholder="Search">
+            <input class="search-bar" name="filter" v-on:keyup="Search(Message)" v-model="Message" placeholder="Search">
             <input class="clear-btn" type="reset" value="Clear" v-on:click="Reset()">
         </form>
     </div>
@@ -9,6 +9,38 @@
 
 <script>
 import Vue from 'vue';
+
+//Functions ..
+let Highlight = function (query) {
+    let queryReg = new RegExp(query, 'i');
+
+    //Check employee names
+    document.querySelectorAll('.employee-name').forEach(function (el) {
+        if (queryReg.test(el.textContent)) {
+            el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
+        }
+    });
+    //Check employee address
+    document.querySelectorAll('.employee-address').forEach(function (el) {
+        if (queryReg.test(el.textContent)) {
+            el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
+        }
+    });
+    //Check employee About
+    document.querySelectorAll('.employee-about').forEach(function (el) {
+        if (queryReg.test(el.textContent)) {
+            el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
+        }
+    });
+}
+
+let ClearHighlight = function () {
+    //give all highlighted elements white background
+    document.querySelectorAll('.highlight').forEach(function (el) {
+        el.style.backgroundColor = "white";
+        el.style.color = "#445065";
+    });
+}
 
 export default {
     name: "filterTable",
@@ -20,49 +52,26 @@ export default {
         }
     },
     methods: {
-        Highlight: function (query) {
-            let queryReg = new RegExp(query, 'i');
-
-            //Check employee names
-            document.querySelectorAll('.employee-name').forEach(function (el) {
-                if (queryReg.test(el.textContent)) {
-                    el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
-                }
-            });
-            //Check employee address
-            document.querySelectorAll('.employee-address').forEach(function (el) {
-                if (queryReg.test(el.textContent)) {
-                    el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
-                }
-            });
-            //Check employee About
-            document.querySelectorAll('.employee-about').forEach(function (el) {
-                if (queryReg.test(el.textContent)) {
-                    el.innerHTML = el.textContent.replace(queryReg, '<span class="highlight">$&</span>');
-                }
-            });
-        },
-        ClearHighlight: function () {
-            //give all highlighted elements white background
-            document.querySelectorAll('.highlight').forEach(function (el) {
-                el.style.backgroundColor = "white";
-                el.style.color = "#445065";
-            });
-        },
         Search: function (query) {
-            this.ClearHighlight();
-            this.Highlight(query);
+            ClearHighlight();
+            Highlight(query);
         },
         Reset: function () {
-            this.ClearHighlight();
-            this.$router.replace('/');
+            ClearHighlight();
+            window.history.pushState({}, document.title, window.location.pathname);
+
         }
     },
     mounted() {
-        console.log("Search Component Rendered")
+        window.addEventListener("load", function (event) {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const filterQuery = urlParams.get('filter')
+            setTimeout(() => {
+                Highlight(filterQuery);
+            }, 1000)
+        })
     }
-
 };
-
 
 </script>
