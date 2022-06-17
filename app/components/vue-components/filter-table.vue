78f3503d053be8dcde1f2ed09aaf8aa85db72e83
Hussien Kamal <Hussien.Kamal@linkdev.com>
2022-06-17 19:41:43 +0200

    create table filter component

---
+++ app/components/vue-components/filter-table.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="demo">
+        <form id="search">
+            Search <input name="query" v-on:keyup="Search(Message)">
+            <input type="reset">
+        </form>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+
+export default {
+    name: "filterTable",
+    components: {
+    },
+    data() {
+        return {
+            Message: ''
+        }
+    },
+    methods: {
+        Search: function (query) {
+            let contentText = "";
+            contentText = document.querySelectorAll('.employee-name').forEach(function (el) {
+                console.log(el.textContent);
+            })
+            console.log(contentText);
+            // var iQuery = new RegExp(query, "ig");
+            // return words.toString().replace(iQuery, function (matchedTxt, a, b) {
+            //     return ('<span class=\'highlight\'>' + matchedTxt + '</span>');
+            // });
+        },
+        Reset: function () {
+
+        }
+    },
+    mounted() {
+        console.log("Search Component Rendered")
+    }
+
+};
+
+
+</script>
